7e7398ba0536345232a7a58ac46f081e7c20715e
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-02 16:05:33 -0700

    test 10 is passing

--- exercise_10/exercise-10.sql
+++ exercise_10/exercise-10.sql
@@ -1,2 +1,10 @@
 -- find the category name of all inventory items that have never been rented
 -- Similar to exercise 9 with an additonal join
+
+select category.name
+from category
+left join film_category on category.category_id = film_category.category_id
+left join film on film_category.film_id = film.film_id
+inner join inventory on film.film_id = inventory.film_id
+left join rental on inventory.inventory_id = rental.inventory_id
+where rental.rental_id is null
--- exercise_10/exercise-10.test.js
+++ exercise_10/exercise-10.test.js
@@ -4,7 +4,7 @@ const pool = require('../lib/utils/pool');
 describe('exercise 10', () => {
   afterAll(() => pool.end());
 
-  it.skip('find the category name of all inventory items that have never been rented', async () => {
+  it('find the category name of all inventory items that have never been rented', async () => {
     const { rows } = await pool.query(
       fs.readFileSync(`${__dirname}/exercise-10.sql`, 'utf-8')
     );
