aa18f2c5757206a65fb15d5c2fe3bc2073c29530
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-02 16:28:28 -0700

    test 11 passing

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,11 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+
+select film.title, count(rental.rental_date)
+from film
+left join inventory on film.film_id = inventory.film_id
+left join rental on inventory.inventory_id = rental.inventory_id
+group by film.film_id
+order by count desc 
+limit 10
