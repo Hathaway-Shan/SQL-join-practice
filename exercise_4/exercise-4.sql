1b13adb47a11ce7cf6fbc20b672600ba5ac4acb2
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-02 14:27:37 -0700

    test 4 is passing

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -1,2 +1,7 @@
 -- find all customers first_name, last_name, address, and city
 -- Similar to exercise 3 but with customer, address, city
+
+select customer.first_name, customer.last_name, address.address, city.city
+from customer
+left join address on address.address_id = customer.address_id
+left join city on city.city_id = address.city_id
