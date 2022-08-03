ab601204dbcecefcaf2ef9be80cf97c24cf02048
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-02 17:04:51 -0700

    test 14 is passing

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,8 @@
 -- the average film length by category
 -- include the category name and avg length
+
+select category.name, avg(film.length)
+from category
+left join film_category on category.category_id = film_category.category_id
+left join film on film_category.film_id = film.film_id
+group by category.name
